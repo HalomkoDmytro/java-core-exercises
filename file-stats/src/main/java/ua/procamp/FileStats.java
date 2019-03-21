bccda4d875bb03046dfe10b1e90c45c2b50ae62e
Dmytro Halomko <galomko.dmytro@gmail.com>
2019-03-21 23:53:38 +0200

    API that allow to get character statistic based on TEXT file by Halomko Dependent from file-reader.

--- file-stats/src/main/java/ua/procamp/FileStats.java
+++ file-stats/src/main/java/ua/procamp/FileStats.java
@@ -1,46 +1,80 @@
 package ua.procamp;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 /**
- * {@link FileStats} provides an API that allow to get character statistic based on text file. All whitespace characters
+ * {@link FileStats} provides an API that allow to get character statistic based on TEXT file. All whitespace characters
  * are ignored.
  */
 public class FileStats {
+
+    private final String TEXT;
+
+    private FileStats(String fileName) {
+        TEXT = Optional
+                .ofNullable(FileReaders.readWholeFile(fileName))
+                .orElseThrow(() -> new FileStatsException("Can't get file"));
+    }
+
     /**
-     * Creates a new immutable {@link FileStats} objects using data from text file received as a parameter.
+     * Creates a new immutable {@link FileStats} objects using data from TEXT file received as a parameter.
      *
-     * @param fileName input text file name
-     * @return new FileStats object created from text file
+     * @param fileName input TEXT file name
+     * @return new FileStats object created from TEXT file
      */
     public static FileStats from(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        return new FileStats(fileName);
     }
 
     /**
      * Returns a number of occurrences of the particular character.
      *
      * @param character a specific character
-     * @return a number that shows how many times this character appeared in a text file
+     * @return a number that shows how many times this character appeared in a TEXT file
      */
     public int getCharCount(char character) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        return (int) TEXT.chars()
+                .filter(ch -> ch == character)
+                .count();
     }
 
     /**
-     * Returns a character that appeared most often in the text.
+     * Returns a character that appeared most often in the TEXT.
      *
      * @return the most frequently appeared character
      */
     public char getMostPopularCharacter() {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        String ch = Stream.of(TEXT.toLowerCase().split(""))
+                .filter(x -> x.matches("[A-Za-z]"))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .findAny()
+                .map(entry -> entry.getKey())
+                .orElseThrow(() -> new FileStatsException("File is empty"));
+        return ch.charAt(0);
     }
 
     /**
-     * Returns {@code true} if this character has appeared in the text, and {@code false} otherwise
+     * Returns {@code true} if this character has appeared in the TEXT, and {@code false} otherwise
      *
      * @param character a specific character to check
-     * @return {@code true} if this character has appeared in the text, and {@code false} otherwise
+     * @return {@code true} if this character has appeared in the TEXT, and {@code false} otherwise
      */
     public boolean containsCharacter(char character) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        Objects.requireNonNull(character);
+        return TEXT.toLowerCase()
+                .chars()
+                .filter(ch -> ch >= 'a' && ch <= 'z')
+                .anyMatch(ch -> ch == character);
     }
+
 }
